c924ddae9c108b4c3794a61d5759fb9b36894c45
rauch <rauchrodrigues@gmail.com>
2023-05-15 16:33:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+  let [coods, setCoods] = useState({ latitude: "", longitude: "" });
+  let { latitude, longitude } = coods;
+  let [month, setMonth] = useState(new Date().getMonth() + 1);
+  let [ans, setAns] = useState("");
+
+  let montharr = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+  function setcoodsfunc() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        let obj = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        }
+        console.log(position);
+        setCoods(obj);
+        setansfunc(obj.latitude);
+      });
+    }
+  }
+
+  function setansfunc(latitude) {
+      if(latitude>0){
+        if(month>=10 || month<=3){
+          setAns("Its chilly in Northern Hemisphere");
+        }
+        else if(month>=3 && month<=10){
+          setAns("Lets hit the beach in Northern Hemisphere");
+        }
+      }
+      else{
+        if(month>=10 || month<=3){
+          setAns("Lets hit the beach in Southern Hemisphere");
+        }
+        else if(month>=3 && month<=10){
+          setAns("Its chilly in Southern Hemisphere");
+        }
+      }
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>lat: {latitude}, long: {longitude}</h1>
+      <button onClick={setcoodsfunc}>Find Loction</button>
+
+      <h2>{latitude > 0 ? "Northern Hemisphere" : latitude == "" ? "Equator" : "Southern Hemisphere"}</h2>
+      <h2>{montharr[month - 1]}</h2>
+      <h2>{ans}</h2>
+    </>
   );
 }
 
